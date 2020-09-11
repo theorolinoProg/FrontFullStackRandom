2257b5b7724346f6b96087f5a7eeb0573c2d20d0
Wirus <louis.wag22@gmail.com>
2020-09-11 16:06:23 +0200

    Create-react-app + TypeScript + Eslint + Prettier configured

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	extends: [
+		'airbnb-typescript',
+		'airbnb/hooks',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:jest/recommended',
+		'prettier',
+		'prettier/react',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended'
+	],
+	plugins: [
+		'react',
+		'prettier',
+		'@typescript-eslint',
+		'jest'
+	],
+	env: {
+		browser: true,
+		es6: true,
+		jest: true,
+	},
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	rules: {
+		'@typescript-eslint/no-empty-interface': 'off',
+		'@typescript-eslint/no-redeclare': 'off',
+		'@typescript-eslint/no-shadow': 'off',
+		'linebreak-style': 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		]
+	},
+};
